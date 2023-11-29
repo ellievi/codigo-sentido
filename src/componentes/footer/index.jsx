c59e07e85e3caa7a2ca7433c432e7f7581b8e818
ellievi <residentamv@hotmail.com>
2023-11-28 21:42:51 -0300

    criacao do componente de footer

---
+++ src/componentes/footer/index.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="flex justify-center p-10 font-sans">
+      <p className="max-w-xl text-white">
+        © Código Sentido 2023
+        <br/>
+        ESTE PROJETO FAZ PARTE DE UMA ATIVIDADE EXTENSIONISTA PARA O CURSO DE 
+        ANÁLISE E DESENVOLVIMENTO DE SISTEMAS DA UNINTER.
+        </p>
+    </div>
+  );
+}
+
+export default Footer;
