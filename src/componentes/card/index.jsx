7dc343f1a3dde35c782e657f0933961b3c04196f
ellievi <residentamv@hotmail.com>
2023-12-06 22:06:05 -0300

    reajuste de logica para o componente card

--- src/componentes/card/index.jsx
+++ src/componentes/card/index.jsx
@@ -1,26 +1,22 @@
 import React from "react";
 import Button from "../button";
+import PropTypes from 'prop-types';
 
-const Card = () => {
+const Card = (props, onClick) => {
   return (
-    <div className="grid grid-cols-3 gap-10 p-10">
-      <div className="bg-gradient-purple p-6">
-        <h4 className="font-semibold text-xl text-white">Oi</h4>
-        <p className="p-4 text-white">Descrição do curso</p>
-        <Button title="Redirecionar ao site"/>
-      </div>
-      <div className="bg-gradient-purple p-6">
-        <h4 className="font-semibold text-xl text-white">Oi</h4>
-        <p className="p-4 text-white">Descrição do curso</p>
-        <Button title="Redirecionar ao site"/>
-      </div>
-      <div className="bg-gradient-purple p-6">
-        <h4 className="font-semibold text-xl text-white">Oi</h4>
-        <p className="p-4 text-white">Descrição do curso</p>
-        <Button title="Redirecionar ao site"/>
-      </div>
+    <div className="grid bg-gradient-purple p-6">
+      <h4 className="font-semibold text-xl text-white">{props.title}</h4>
+      <p className="p-4 text-white">{props.description}</p>
+      {props.buttonTitle && (
+        <Button onClick={() => onClick} title={props.buttonTitle}/>
+      )}
+      {props.children}
     </div>
   );
+}
+
+Button.propTypes = {
+  onClick: PropTypes.func
 }
 
 export default Card;
