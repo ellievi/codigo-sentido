ab8cfe80e3fa4d9330e9506a84a2e057f7aeed42
ellievi <residentamv@hotmail.com>
2023-12-06 22:02:35 -0300

    criacao do componente de tela inicial

---
+++ src/pages/home/index.jsx
@@ -0,0 +1,17 @@
+import Menu from '../../componentes/menu';
+import Banner from '../../componentes/banner';
+import SectionList from '../../componentes/sectionsList';
+import Footer from '../../componentes/footer';
+
+const Home = () => {
+  return (
+    <div>
+      <Menu/>
+      <Banner/>
+      <SectionList/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default Home;
