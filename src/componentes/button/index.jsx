341571e434f7436c31ea891e44d8942ef425925a
ellievi <residentamv@hotmail.com>
2023-12-06 22:05:42 -0300

    adicao de logica para ancoragem de links para botao

--- src/componentes/button/index.jsx
+++ src/componentes/button/index.jsx
@@ -1,16 +1,19 @@
 import React from "react";
 import PropTypes from 'prop-types';
 
-const Button = (props) => {
+const Button = (props, onClick) => {
   return (
     <div className="flex justify-center p-4 font-sans">
-      <button className="w-60 h-12 bg-light-green border-4 border-purple-350 rounded-full font-semibold">{props.title}</button>
+      <button onClick={() => onClick} className="w-60 h-12 bg-light-green border-4 border-purple-350 rounded-full font-semibold">
+        <a href={props.link} onClick={window.history.pushState("", "", "/")}>{props.title}</a>
+      </button>
     </div>
   );
 }
 
 Button.protoTypes = {
-  title: PropTypes.string
+  title: PropTypes.string,
+  onClick: PropTypes.func
 }
 
 export default Button;
