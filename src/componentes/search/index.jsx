f3d19bc12bbd0d920e7bccfb59df101c2133849d
ellievi <residentamv@hotmail.com>
2023-12-20 19:41:08 -0300

    adicao do componente TextInput

--- src/componentes/search/index.jsx
+++ src/componentes/search/index.jsx
@@ -1,15 +1,18 @@
 import React from "react";
 import PesquisaIcone from '../../assets/pesquisa_icon.svg'
+import TextInput from "../textInput";
 
 const Search = () => {
   return (
-    <div>
-      <input className="w-52 text-lg bg-lightest-green rounded-xl outline-0"/>
-      <img
-          className="w-6"
-          src={PesquisaIcone} 
-          alt="Ícone de busca representado por uma lupa."
-          />
+    <div className="flex flex-row">
+      <TextInput placeholder="Digite sua pesquisa"/>
+      <button className="bg-light-purple py-2 h-8 rounded-md">
+        <img
+            className="w-6"
+            src={PesquisaIcone} 
+            alt="Ícone de busca representado por uma lupa."
+            />
+      </button>
     </div>
   );
 }
