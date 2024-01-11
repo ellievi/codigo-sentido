4cfcfe627aa397623659a555d9aeb5d09fdbc219
ellievi <residentamv@hotmail.com>
2024-01-11 19:32:08 -0300

    ajuste de logica para links

--- src/components/button/index.jsx
+++ src/components/button/index.jsx
@@ -5,7 +5,7 @@ const Button = (props, onClick) => {
   return (
     <div className="flex justify-center p-4 font-sans">
       <button role="button" onClick={() => onClick} className="w-60 h-12 bg-light-green border-4 border-purple-350 rounded-full font-semibold">
-        <a href={props.link} target="_blank" rel="noreferrer">{props.title}</a>
+        <a href={props.link} target={props.samePage ? '' : "_blank"} rel="noreferrer">{props.title}</a>
       </button>
     </div>
   );
